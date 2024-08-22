e535661e8c088b329cf8d04821f33df93e3a187d
rstlix0x0 <rstlix.dev@gmail.com>
2024-08-22 09:27:58 +0700

    refactor(client): adding methods to JSONResponse

--- client/src/types.rs
+++ client/src/types.rs
@@ -15,6 +15,12 @@ pub enum ExecutorError {
 
     #[error("executor error: build value error: {0}")]
     BuildValueError(String),
+
+    #[error("executor error: missing error")]
+    MissingError,
+
+    #[error("executor error: missing result")]
+    MissingResult,
 }
 
 /// `RpcValue` used to convert any value types to the
@@ -39,6 +45,30 @@ where
     pub error: Option<RpcErrorBuilder<E>>,
 
     pub id: Option<RpcId>,
+}
+
+impl<T, E> JSONResponse<T, E>
+where
+    T: Clone,
+    E: Clone,
+{
+    pub fn is_error(&self) -> bool {
+        self.error.is_some()
+    }
+
+    pub fn extract_err(&self) -> Result<RpcErrorBuilder<E>, ExecutorError> {
+        match &self.error {
+            Some(err) => Ok(err.to_owned()),
+            None => Err(ExecutorError::MissingError)
+        }
+    }
+
+    pub fn extract_result(&self) -> Result<T, ExecutorError> {
+        match &self.result {
+            Some(result) => Ok(result.to_owned()),
+            None => Err(ExecutorError::MissingResult) 
+        }
+    }
 }
 
 /// `Executor` is a main interface that need to implement by
